32773bfe69680360c0523b295227b98c08c2986a
Margaret Sun <hi.margaret.sun@gmail.com>
2024-07-01 15:58:45 -0700

    setup expo router

--- src/components/ExerciseListItem.jsx
+++ src/components/ExerciseListItem.jsx
@@ -18,6 +18,16 @@ export default function ExerciseListItem({ item }) {
       padding: 10,
       borderRadius: 10,
       gap: 5,
+
+      //shadow
+      shadowColor: "#000000",
+      shadowOffset: {
+        width: 0,
+        height: 1,
+    },
+      shadowOpacity:  0.16,
+      shadowRadius: 1.51,
+      elevation: 2
     },
     exerciseName: {
       fontSize: 20,
