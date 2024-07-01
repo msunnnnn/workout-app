3bdaee50ccb052d1d6e22922c731eddffa612d27
Margaret Sun <hi.margaret.sun@gmail.com>
2024-07-01 16:53:24 -0700

    details screen for exercise

--- src/components/ExerciseListItem.jsx
+++ src/components/ExerciseListItem.jsx
@@ -1,40 +1,43 @@
-import { StyleSheet, Text, View,  } from "react-native";
+import { StyleSheet, Text, View, Pressable } from "react-native";
+import { Link } from "expo-router";
 
 export default function ExerciseListItem({ item }) {
-    return (
-      <View style={styles.exerciseContainer}>
+  return (
+    <Link href={`/${item.name}`} asChild>
+      <Pressable style={styles.exerciseContainer}>
         <Text style={styles.exerciseName}>{item.name}</Text>
         <Text style={styles.exercisesSubtitle}>
-          <Text style={styles.subValue}>{item.muscle}</Text> | <Text style={styles.subValue} >{item.equipment}</Text>
+          <Text style={styles.subValue}>{item.muscle}</Text> |{" "}
+          <Text style={styles.subValue}>{item.equipment}</Text>
         </Text>
-      </View>
-    );
-  }
+      </Pressable>
+    </Link>
+  );
+}
 
+const styles = StyleSheet.create({
+  exerciseContainer: {
+    backgroundColor: "#fff",
+    padding: 10,
+    borderRadius: 10,
+    gap: 5,
 
-  const styles = StyleSheet.create({
-    exerciseContainer: {
-      backgroundColor: "#fff",
-      padding: 10,
-      borderRadius: 10,
-      gap: 5,
-
-      //shadow
-      shadowColor: "#000000",
-      shadowOffset: {
-        width: 0,
-        height: 1,
+    //shadow
+    shadowColor: "#000000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
     },
-      shadowOpacity:  0.16,
-      shadowRadius: 1.51,
-      elevation: 2
-    },
-    exerciseName: {
-      fontSize: 20,
-      fontWeight: "500",
-    },
-    exercisesSubtitle: { color: "dimgray" },
-    subValue:{
-        textTransform: 'capitalize'
-    }
-  });
+    shadowOpacity: 0.16,
+    shadowRadius: 1.51,
+    elevation: 2,
+  },
+  exerciseName: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  exercisesSubtitle: { color: "dimgray" },
+  subValue: {
+    textTransform: "capitalize",
+  },
+});
